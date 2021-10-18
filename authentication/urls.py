1e0a487750cead3fc4ff34c230701ed55d8e06f7
PavelFihuryn <pavelfihuryn@gmail.com>
2021-10-18 08:34:08 +0300

    refactor refresh url

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,6 +8,7 @@ from authentication.views import (
     PasswordTokenCheckAPIView,
     RequestPasswordResetEmail,
     SetNewPasswordAPIView, LogoutAPIView,
+    CookieTokenRefreshView,
 )
 
 urlpatterns = [
@@ -15,7 +16,7 @@ urlpatterns = [
     path('email-verify/', VerifyEmailAPIView.as_view(), name='email-verify'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('password-reset-email/', RequestPasswordResetEmail.as_view(), name='password-reset-email'),
     path('password-reset/<uidb64>/<token>/', PasswordTokenCheckAPIView.as_view(), name='password-reset-confirm'),
     path('password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
