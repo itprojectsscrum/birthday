e7d88e090d3fa836c88525be5cfabad91bdb9c16
PavelFihuryn <pavelfihuryn@gmail.com>
2021-10-19 23:31:14 +0300

    domain

--- birthday_1/settings.py
+++ birthday_1/settings.py
@@ -234,9 +234,9 @@ SIMPLE_JWT = {
 
     # custom
     'AUTH_COOKIE': 'refresh_token',  # Cookie name. Enables cookies if value is set.
-    'AUTH_COOKIE_DOMAIN': None,     # A string like "example.com", or None for standard domain cookie.
+    'AUTH_COOKIE_DOMAIN': 'congratulations-two.vercel.app',     # A string like "example.com", or None for standard domain cookie.
     'AUTH_COOKIE_SECURE': True,    # Whether the auth cookies should be secure (https:// only).
-    'AUTH_COOKIE_HTTP_ONLY': False,  # Http only cookie flag.It's not fetch by javascript.
+    'AUTH_COOKIE_HTTP_ONLY': True,  # Http only cookie flag.It's not fetch by javascript.
     'AUTH_COOKIE_PATH': '/',        # The path of the auth cookie.
     'AUTH_COOKIE_SAMESITE': 'None',  # Whether to set the flag restricting cookie leaks on cross-site requests. This can be 'Lax', 'Strict', or None to disable the flag.
 
