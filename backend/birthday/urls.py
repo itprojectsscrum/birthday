31aadd82ab9a2492f2b2dd3ed4a6618adbc3088a
PavelFihuryn <pavelfihuryn@gmail.com>
2021-12-01 08:41:15 +0300

    url space

--- backend/birthday/urls.py
+++ backend/birthday/urls.py
@@ -22,8 +22,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
-    path('congratulations/', include('congratulation.urls')),
+    path('api/v1/auth/', include('authentication.urls')),
+    path('api/v1/congratulations/', include('congratulation.urls')),
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
