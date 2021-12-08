1587d772b3e35c9471009af22caf5d1372218c14
PavelFihuryn <pavelfihuryn@gmail.com>
2021-12-08 21:46:43 +0300

    location

--- src/core/constants.tsx
+++ src/core/constants.tsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://bdaybook.ru/api/v1/'
+export const BASE_URL = 'http://127.0.0.1:8080/api/v1/'//'https://bdaybook.ru/api/v1/'
 export const EMAIL_DEFAULT = ""//"evgenicuss@gmail.com"
 export const PASSWORD_DEFAULT = ""//"123212d"
 export const MIN_TIME_OF_LIVE_ACCESS_TOKEN = 65000
